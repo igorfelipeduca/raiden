ad437de13a1823f8b409c170b69d114727fb0ed8
Duca <yelldutz@gmail.com>
2024-05-27 15:01:43 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_SUPABASE_URL: "https://johdvypbxsyngfdiocyf.supabase.co",
+    NEXT_SUPABASE_URL: "",
     NEXT_SUPABASE_ANON_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvaGR2eXBieHN5bmdmZGlvY3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc2MDA2ODMsImV4cCI6MjAxMzE3NjY4M30.-UvrWgdMIT04W-2XiR-1uqW3ZcJ9dfrJZnSR4SWZ7kI",
+      "",
   },
   async headers() {
     return [
